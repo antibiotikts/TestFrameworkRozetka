d50d79aac5a2fa32512a14e745122e0ac062a5a2
antibiotikts <107547014+antibiotikts@users.noreply.github.com>
2022-12-27 17:22:55 +0200

    add method closeBrowser in class BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import general.GeneralActions;
+import org.junit.After;
 import org.openqa.selenium.WebDriver;
 import page.BasePage;
 import page.HomePage;
@@ -8,4 +9,8 @@ public class BaseTest {
     protected BasePage basePage = new BasePage(driver);
     protected HomePage homePage = new HomePage(driver);
 
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
 }
